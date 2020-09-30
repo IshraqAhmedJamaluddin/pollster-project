e994a7fa6336883c83c47d47f89ce4783cd618de
IshraqAhmedJamaluddin <46565672+IshraqAhmedJamaluddin@users.noreply.github.com>
2020-09-30 23:50:00 +0200

    Update views.py

--- pollster/polls/views.py
+++ pollster/polls/views.py
@@ -19,10 +19,12 @@ def detail(request, question_id):
         raise Http404("Question does not exist")
     return render(request, 'polls/detail.html', {'question': question})
 
+
 # Get questions and display results
 def results(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/results.html',{'question':question})
+
 
 # Vote for a question choice
 def vote(request, question_id):
